8dc8052a85ac6ad8e3ec5193804ee7c3192ef2b0
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-23 21:29:03 +0100

    initial commit

---
+++ update_module.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module_name='choria-mcollective_data_sysctl'
+module_version='3.0.0'
+
+echo 'Updating module'
+
+wget -c "https://forge.puppet.com/v3/files/${module_name}-${module_version}.tar.gz" -O - | tar -xz
+
+mv ${module_name}-${module_version}/* .
+
+rm -rfv "${module_name}-${module_version}"
